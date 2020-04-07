7fab0db27cc8bda113f4c0159067e6f529e06715
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-07 10:20:12 +0200

    rebuild docs

---
+++ docs/precache-manifest.333fbaa754f447e20dd4e5ceb33d9846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0096483a62fcf24907d468f30366483f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa5bc6814583b0b98276",
+    "url": "/static/css/main.bcebed1f.chunk.css"
+  },
+  {
+    "revision": "28aec539e86a15fda2cc",
+    "url": "/static/js/2.0f738294.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f738294.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fa5bc6814583b0b98276",
+    "url": "/static/js/main.614dce7e.chunk.js"
+  },
+  {
+    "revision": "f092b7790602b3d591a1",
+    "url": "/static/js/runtime-main.2b0b8e08.js"
+  }
+]);
