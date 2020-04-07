0058ace974450706418a4a861b40976a07b720bb
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-07 08:25:18 +0200

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../stylesheets/App.scss';
+
+class App extends React.Component {
+ const alertButton = (
+  <button onClick={ /* aquí va la función */ }>
+  Pedir más información
+</button>
+);
+}
+
+export default App;
