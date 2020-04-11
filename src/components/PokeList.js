e0d79d06bfe9fb03f769479bf5dc6a6a05259576
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-11 20:05:39 +0200

    #1 / set function to iterate array

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -2,10 +2,13 @@ import React from 'react';
 import '../stylesheets/App.scss';
 
 const PokeList = (props) => {
-  console.log(props);
+  console.log(props.pokemon);
+  const htmlCode = props.pokemon.map((pokemon) => {
+    console.log('holiiiiii');
+  });
   return (
     <div>
-      <ul className="poke-list">Lista Pokemon</ul>
+      <ul className="poke-list">{htmlCode}</ul>
     </div>
   );
 };
