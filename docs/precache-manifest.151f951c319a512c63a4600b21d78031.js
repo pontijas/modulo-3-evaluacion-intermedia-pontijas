d9b099d127a13540e275663cd60ca7798ee76f24
cristinapontiijas <cristinapontijas@gmail.com>
2020-04-18 17:04:38 +0200

    npm run build and change name to docs

---
+++ docs/precache-manifest.151f951c319a512c63a4600b21d78031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed3d37d1578870211231f7ba481c50c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cb0c98a46fbfeb7328dd",
+    "url": "./static/css/main.2d958e8d.chunk.css"
+  },
+  {
+    "revision": "3e644609208c45cf3969",
+    "url": "./static/js/2.80f47701.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.80f47701.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb0c98a46fbfeb7328dd",
+    "url": "./static/js/main.6a02787b.chunk.js"
+  },
+  {
+    "revision": "cbb7502546e28b5dbd02",
+    "url": "./static/js/runtime-main.5c2b7656.js"
+  }
+]);
